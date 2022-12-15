05ed4c3b799ae310b3c285280a4c9558e49eed20
alejandropmz <alejandropacheco2497@gmail.com>
2022-12-15 03:32:09 +0000

    commit

--- .learn/resets/10-Array-Methods/app.py
+++ .learn/resets/10-Array-Methods/app.py
@@ -1,7 +1,4 @@
 names = ['John', 'Kenny', 'Tom', 'Bob', 'Dilan']
 ## CREATE YOUR FUNCTION HERE
 
-def sort_names(value):
-    return sorted(value)
-
 print(sort_names(names))
--- exercises/06-lambda-functions/app.py
+++ exercises/06-lambda-functions/app.py
@@ -1,2 +1,16 @@
 # your function here
 
+is_odd = lambda value: True if value%2 != 0 else False
+    
+
+
+
+
+
+""" def is_odd(value):
+    if value%2 != 0:
+        print(True)
+    else:
+        print(False)
+
+print(is_odd(5)) """
